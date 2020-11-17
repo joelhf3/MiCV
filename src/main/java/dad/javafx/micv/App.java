af534e4c8f59416b49e326c8f1a086dec492401b
fvarrui <4354486+fvarrui@users.noreply.github.com>
2020-11-17 11:41:08 +0000

    U first commit

---
+++ src/main/java/dad/javafx/micv/App.java
@@ -0,0 +1,29 @@
+package dad.javafx.micv;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
+
+public class App extends Application {
+	
+	private MainController controller;
+
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		controller = new MainController();
+		
+		Scene escena = new Scene(controller.getView());
+		
+		primaryStage.setScene(escena);
+		primaryStage.setTitle("MiCV");
+		primaryStage.getIcons().add(new Image("/images/cv64x64.png"));
+		primaryStage.show();
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+
+}
