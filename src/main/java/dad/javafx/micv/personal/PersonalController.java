120c4dcbecf5351963cccc3d40edfe6047c729c4
fvarrui <4354486+fvarrui@users.noreply.github.com>
2020-11-18 10:06:15 +0000

    A personal controller

---
+++ src/main/java/dad/javafx/micv/personal/PersonalController.java
@@ -0,0 +1,80 @@
+package dad.javafx.micv.personal;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import dad.javafx.micv.model.Personal;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.GridPane;
+
+public class PersonalController implements Initializable {
+
+	// model
+
+	private ObjectProperty<Personal> personal = new SimpleObjectProperty<>();
+
+	// view
+
+	@FXML
+	private GridPane view;
+
+	@FXML
+	private TextField identificacionText;
+
+	@FXML
+	private TextField nombreText;
+
+	@FXML
+	private TextField apellidosText;
+
+	public PersonalController() throws IOException {
+		FXMLLoader loader = new FXMLLoader(getClass().getResource("/fxml/PersonalView.fxml"));
+		loader.setController(this);
+		loader.load();
+	}
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		
+		// TODO cargar el combo de paises
+
+		this.personal.addListener((o, ov, nv) -> onPersonalChanged(o, ov, nv));
+		
+	}
+
+	private void onPersonalChanged(ObservableValue<? extends Personal> o, Personal ov, Personal nv) {
+		
+		if (ov != null) {
+			identificacionText.textProperty().unbindBidirectional(ov.identificacionProperty());
+			nombreText.textProperty().unbindBidirectional(ov.nombreProperty());
+			apellidosText.textProperty().unbindBidirectional(ov.apellidosProperty());			
+		}
+
+		if (nv != null) {
+			identificacionText.textProperty().bindBidirectional(nv.identificacionProperty());
+			nombreText.textProperty().bindBidirectional(nv.nombreProperty());
+			apellidosText.textProperty().bindBidirectional(nv.apellidosProperty());
+		}
+		
+	}
+
+	public final ObjectProperty<Personal> personalProperty() {
+		return this.personal;
+	}
+
+	public final Personal getPersonal() {
+		return this.personalProperty().get();
+	}
+
+	public final void setPersonal(final Personal personal) {
+		this.personalProperty().set(personal);
+	}
+
+}
